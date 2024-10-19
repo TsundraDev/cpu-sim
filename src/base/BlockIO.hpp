272b6fb6359054b51298b38950472df37f5dac30
Tsundra <tsundradev@gmail.com>
2024-10-19 13:02:01 +0700

    Refactor Register, send through BlockIO

--- src/base/BlockIO.hpp
+++ src/base/BlockIO.hpp
@@ -44,8 +44,10 @@ public:
   void set(T val) { m_val = val; }
 
   void send(Agent* sender, uint64_t tick) {
+    T* send_data = new T;
+    *send_data = m_val;
     for (uint64_t i = 0; i < m_dest.size(); i++) {
-      sender->createEvent(Event(tick, sender, m_dest[i]));
+      sender->createEvent(Event(tick, sender, m_dest[i], (uint8_t*)send_data, sizeof(m_val)));
     }
   }
 
--- src/components/Register.cpp
+++ src/components/Register.cpp
@@ -27,18 +27,10 @@ void Register::updateInput(Agent* src, uint8_t* data, uint64_t size) {
 void Register::updateBlock() {
   uint64_t input = m_input.get();
   m_output.set(input);
+  printf("Reg : %lx\n", input);
 }
 
 void Register::updateOutput() {
   uint64_t cur_tick = m_event_queue->tick();
-  uint64_t* send_data = new uint64_t;
-  *send_data = m_output.get();
-  for (uint64_t i = 0; i < m_output.getDestination().size(); i++) {
-    this->createEvent(Event(cur_tick,
-                            (Agent*)this,
-                            (Agent*)m_output.getDestination()[i],
-                            (uint8_t*)send_data,
-                            8)
-    );
-  }
+  m_output.send(this, cur_tick);
 }
